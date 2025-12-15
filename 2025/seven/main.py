2ddef74dc9f311dc7033cb622445f8b00046a400
Vanessa Roque <viroque19@gmail.com>
2025-12-14 20:57:17 -0800

    add files for day 7

---
+++ 2025/seven/main.py
@@ -0,0 +1,41 @@
+from icecream import ic
+
+small = "small.txt"
+big = "large.txt"
+AOC_CONFIG = {
+    "part" : 2, # 0, 1,
+    "input": big
+}
+def read_input(file: str) -> list[str] | None:
+    try:
+        with open(file, 'r') as f:
+            # Reads all lines and strips leading/trailing whitespace (including newlines)
+            data = [line for line in f.readlines()]
+        return data
+    except FileNotFoundError:
+        print(f"Error: The file was not found.")
+        return None
+
+def part_one(data: list[str]):
+    ...
+def part_two(data: list[str]):
+    ...
+def main():
+    lines = read_input(str(AOC_CONFIG["input"]))
+    if lines is None:
+        print("error reading file")
+        return
+    if AOC_CONFIG["part"] == 0 or AOC_CONFIG["part"] == 1:
+        ans_one = part_one(lines)
+        print(f"Answer 2: {ans_one}")
+
+    if AOC_CONFIG["part"] == 0 or AOC_CONFIG["part"] == 2:
+        ans_two = part_two(lines)
+        print(f"Answer 2: {ans_two}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
